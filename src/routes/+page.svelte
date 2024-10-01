834db5c831f2bc7a5fb579335650368d9d92524c
Cooper <Cooperransom08@gmail.com>
2024-10-01 15:08:41 +0000

    Setup project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  import '../global.css';
+</script>
+
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>

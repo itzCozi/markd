5b991abbdce3666ddee623bd7d5d6c513daed47d
Ivan Evans <74743263+Pasithea0@users.noreply.github.com>
2024-10-02 19:35:38 -0600

    Fixed and made beautiful

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,51 +29,23 @@
   let source: string = localStorageStore.get('markdown') || '';
   setContext("source", source);
 
-  let numberOfLines = source.split('\n').length;
-
-  function handleInput(event: Event) {
-    const target = event.target as HTMLTextAreaElement;
-    const value = target.value;
-    source = value;
-    localStorageStore.set('markdown', value);
-    KeyUp(event);
+  $: {
+    localStorageStore.set('markdown', source);
   }
-
-  const KeyUp = (event: Event) => {
-    const textarea = event.target as HTMLInputElement;
-    numberOfLines = textarea.value.split('\n').length;
-  };
-
-  const KeyDown = (event: Event) => {
-    const textarea = event.target as HTMLInputElement;
-
-    if ((event as KeyboardEvent).key === 'Tab') {
-      const start = textarea.selectionStart;
-      const end = textarea.selectionEnd;
-
-      if (start && end) {
-        textarea.value = textarea.value.substring(0, start) + '\t'
-        + textarea.value.substring(end);
-        event.preventDefault();
-      }
-    }
-  };
 
   const carta = new Carta({
     sanitizer: DOMPurify.sanitize,
-    theme: 'carta-dark'
+    theme: 'github-dark'
   });
+
 </script>
 
 <div class="flex h-screen bg-mono-background">
   <div style="width: {leftWidth}%;">
     <div class="flex h-full overflow-hidden">
-      <div class="p-2 text-gray-600 text-right border-r border-[#252525] w-12 line-numbers">
-        {#each Array(numberOfLines) as _}
-                <span class="new-line" />
-        {/each}
+      <div class="p-2 text-gray-600 text-right border-r border-[#252525] w-12">
       </div>
-      <div class="text-content w-full p-2 border-none outline-none resize-none bg-mono-background font-mono overflow-y-auto">     
+      <div class="w-full p-2 border-none outline-none resize-none bg-mono-background font-mono overflow-y-auto">     
         <MarkdownEditor {carta} bind:value={source} />
       </div>
     </div>

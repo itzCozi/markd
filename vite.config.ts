371d99e1af242dd91dce4f4cc15aa595d627170f
Cooper <Cooperransom08@gmail.com>
2024-10-05 20:16:01 +0000

    Try fixing build issues again

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import postcss from './postcss.config.js';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [sveltekit()],
   css: {
-    postcss,
+    postcss
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      '@': '/src',
     },
   },
-  server: {
-    fs: {
-      allow: [
-        // Add the directory containing the favicon.svg file
-        resolve(__dirname, 'static')
-      ]
-    }
-  }
 });

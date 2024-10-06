934868b139e0f81756ac8b36bb25737fadbd7d0b
Ivan Evans <74743263+Pasithea0@users.noreply.github.com>
2024-10-05 23:35:24 -0600

    change sync switch to toolbar style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -102,15 +102,24 @@
   </div>
 
   <div class="renderer p-2 overflow-auto markdown-content" style="width: {100 - leftWidth}%" on:scroll={handleRendererScroll}>
+    <div class="sync-switch">
+      <span class="mr-2 text-white">Sync Scroll</span>
+      <label class="inline-flex items-center">
+      <input 
+        type="checkbox" 
+        bind:checked={isScrollSyncEnabled} 
+        class="hidden"
+      />
+      <span class={`relative inline-block w-8 h-5 transition duration-200 ease-in-out ${isScrollSyncEnabled ? 'bg-[#2E6AAC]' : 'bg-gray-600'} rounded-full cursor-pointer`}>
+        <span class={`absolute top-1 left-1 inline-block w-3 h-3 bg-white rounded-full transition-transform duration-200 ease-in-out ${isScrollSyncEnabled ? "translate-x-3" : ""}`} />
+        <span class={`absolute top-1 left-1 flex items-center justify-center w-3 h-3 transition-transform duration-200 ${isScrollSyncEnabled ? "translate-x-3" : ""}`}>
+          <i class={`fas ${isScrollSyncEnabled ? "fa-check" : "fa-times"} ${isScrollSyncEnabled ? 'text-[#2E6AAC]' : 'text-gray-600'} text-[10px]`}></i>
+        </span>
+      </span>
+    </label>
+  </div>
     {#key source}
       <Markdown {carta} value={source} />
     {/key}
   </div>
-</div>
-
-<!-- Toggle Button for Scroll Sync -->
-<div class="sync-button fixed bottom-4 right-4 p-2">
-  <button on:click={toggleScrollSync} class="px-4 py-2 bg-gray-600 text-white rounded">
-    {isScrollSyncEnabled ? 'Un-Sync' : 'Sync'}
-  </button>
 </div>

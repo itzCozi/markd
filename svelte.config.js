e5615ae40034e97e5d4574027bb5bab7eec9581e
Cooper <Cooperransom08@gmail.com>
2024-10-07 15:42:42 +0000

    build worked

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,12 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+  }
 };
 
 export default config;

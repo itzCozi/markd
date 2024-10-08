1788c582745094185168148a7230bff5932960b7
Cooper <Cooperransom08@gmail.com>
2024-10-08 20:31:40 +0000

    finish most of the usage page

--- src/routes/usage/+page.svelte
+++ src/routes/usage/+page.svelte
@@ -1,7 +1,7 @@
 <!--https://rentry.org/how-->
 
 <div class="flex pt-8 bg-mono-background justify-center items-center">
-  <div class="p-4 bg-mono-accent max-w-3xl rounded-md">
+  <div class="p-4 bg-mono-accent max-w-3xl mb-6 rounded-md">
     <p class="text-3xl font-semibold">Usage</p>
     <p class="text-type-dimmed mt-2">
       Markdown is a lightweight markup language with plain text formatting syntax. Its design allows
@@ -18,7 +18,7 @@
         <code>#</code>
         symbols determines the size of the header. The maximum number of <code>#</code> symbols is 6.
       </p>
-      <pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
+<pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
 # Header 1
 ## Header 2
 ### Header 3
@@ -34,8 +34,20 @@
         Hyperlinks are defined by the text you want to display in square brackets followed by the
         URL in round brackets.
       </p>
-      <pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
+<pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
 [example link](https://example.com)
+</pre>
+    </div>
+
+    <div class="my-4">
+      <p class="text-2xl my-2 text-type-emphasized">Text Styles</p>
+      <p class="text-type-dimmed">
+        Text styles are defined by wrapping the text in asterisks or underscores.
+        Single asterisks is interpreted as italics, double asterisks is interpreted as bold,
+        and triple asterisks is interpreted as bold italics.
+      </p>
+<pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
+*Italics* _Italics_ **Bold** __Bold__ ***Bold Italics*** ___Bold Italics___
 </pre>
     </div>
 
@@ -45,7 +57,7 @@
         Lists are defined using <code>-</code> or <code>*</code> symbols, followed by text. Either a
         hyphen or an asterisk can be used. Numbered lists are defined using numbers followed by a period.
       </p>
-      <pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
+<pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
 - Item 1
 - Item 2
 - Item 3
@@ -62,7 +74,7 @@
         Blockquotes are defined using the <code>&gt;</code> symbol followed by the text you want to standout,
         this is usually used for quotes or as a substitute callouts.
       </p>
-      <pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
+<pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
 > "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
 </pre>
     </div>
@@ -73,7 +85,7 @@
         Code is defined using the <code>`</code> symbol. Inline code is defined by wrapping the code
         in singular backticks. Block code is defined by wrapping the code in triple backticks.
       </p>
-      <pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
+<pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
 `git clone https://github.com/itzcozi/markd.git`
 
 ```
@@ -90,7 +102,7 @@ print(number1 + number2)
         Images are defined similarly to hyperlinks, but with an exclamation mark in front of the
         square brackets and the text in the rounded brackets is the media's web address.
       </p>
-      <pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
+<pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
 ![alt text](https://picsum.photos/200/300)
 </pre>
     </div>
@@ -101,7 +113,7 @@ print(number1 + number2)
         Dividers are defined by three or more hyphens, asterisks, or underscores creating a
         horizontal line.
       </p>
-      <pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
+<pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
 ---
 ***
 ___
@@ -114,10 +126,23 @@ ___
         Tables are defined by using pipes <code>|</code> to separate columns and hyphens
         <code>-</code> to separate the header row from the content.
       </p>
-      <pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
+<pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
 | Header 1 | Header 2 |
 |----------|----------|
 | Cell 1   | Cell 2   |
+</pre>
+    </div>
+
+    <div class="my-4">
+      <p class="text-2xl my-2 text-type-emphasized">Admonitions</p>
+      <p class="text-type-dimmed">
+        Admonitions are defined by using the <code>!!!</code> symbol followed by the
+        type of admonition you want to use and the text you want to display. The admonition types
+        are <code>note</code>, <code>warning</code>, <code>tip</code>, and <code>important</code>.
+      </p>
+<pre class="text-mono bg-mono-card p-3 my-1 rounded-sm text-wrap">
+!!! note Real Debrid Limits
+Keep in mind Real Debrid will only allow devices on the same network as you, Real debrid allows unlimited devices on one IP at a time.
 </pre>
     </div>
 

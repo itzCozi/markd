c81c74c615e4993ceb7c86ae1d5c99635364e8ec
Cooper <Cooperransom08@gmail.com>
2024-10-25 02:11:25 +0000

    fix it!

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import postcss from "./postcss.config.js";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [sveltekit()],
   css: {
     postcss
+  },
+  server: {
+    fs: {
+      allow: [
+        resolve(__dirname, '.'),
+        resolve(__dirname, 'static')
+      ]
+    }
   }
 });

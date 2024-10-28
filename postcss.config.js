b310e3421a364e67d3ccd7810753da6fb115c892
Cooper <Cooperransom08@gmail.com>
2024-10-28 12:00:54 +0000

    update postcss

--- postcss.config.js
+++ postcss.config.js
@@ -2,11 +2,5 @@ import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
 export default {
-  plugins: [
-    tailwindcss({
-      mode: "jit",
-      content: ["./src/**/*.{svelte,js,ts,html}"],
-    }),
-    autoprefixer,
-  ],
+  plugins: [tailwindcss(), autoprefixer],
 };

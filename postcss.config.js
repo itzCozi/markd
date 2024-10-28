6db9ae397580f92778269ac38595b2d04e703f4a
Cooper <Cooperransom08@gmail.com>
2024-10-28 11:58:24 +0000

    update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -5,11 +5,7 @@ export default {
   plugins: [
     tailwindcss({
       mode: "jit",
-      purge: {
-        enabled: true,
-        content: ["./src/**/*.{svelte,html,js,ts}"],
-      },
-      content: ["./src/**/*.{svelte,js,ts}"],
+      content: ["./src/**/*.{svelte,js,ts,html}"],
     }),
     autoprefixer,
   ],

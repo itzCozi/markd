cca0929d871cdea3cca0187eaafe835c44f813ee
Cooper <Cooperransom08@gmail.com>
2024-10-29 14:20:32 +0000

    update the super epic ui

--- src/lib/parts/Stats.svelte
+++ src/lib/parts/Stats.svelte
@@ -8,10 +8,10 @@
   $: readTime = Math.ceil(wordCount / 200);
 </script>
 
-<div class="stats bg-mono-card gap-3 text-type-dimmed text-xs md:ring-1 ring-black ring-opacity-15">
+<div class="stats gap-4 text-type-dimmed text-xs">
   <span class="stat-item">Characters: {characterCount}</span>
   <span class="separator">•</span>
   <span class="stat-item">Words: {wordCount}</span>
   <span class="separator">•</span>
-  <span class="stat-item">Read time: {readTime} {readTime === 1 ? "minute" : "minutes"}</span>
+  <span class="stat-item">Length: {readTime} {readTime === 1 ? "minute" : "minutes"}</span>
 </div>

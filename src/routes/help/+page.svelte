5dc01177ece9e86f3b7a123d359471e8917c6ea7
Cooper <Cooperransom08@gmail.com>
2024-11-12 16:42:08 +0000

    fix a small issue in help page with markdown

--- src/routes/help/+page.svelte
+++ src/routes/help/+page.svelte
@@ -116,9 +116,9 @@ ___" />
   <HelpEntry
     title="Subscript & Superscript"
     description="Subscript is defined using two of the ~ symbol followed by the text you want to be subscripted. Superscript is defined using two the caret ^ symbol followed by the text you want to be superscripted."
-    example="H~2~O is a liquid. 2^10^ is 1024.
+    example={`H~2~O is a liquid. 2^10^ is 1024.
 or
-$H_20$ is a liquid. $2^{10}$ is 1024." />
+$H_20$ is a liquid. $2^{10}$ is 1024.`} />
 
   <HelpEntry
     title="Math"

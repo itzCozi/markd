b74a4317d263670191e5fd4d8bcbd0ecf01f6600
Ivan Evans <74743263+Pasithea0@users.noreply.github.com>
2024-11-17 13:36:05 -0700

    Tweak editor theme setting

--- src/lib/parts/SettingsPane.svelte
+++ src/lib/parts/SettingsPane.svelte
@@ -2,6 +2,16 @@
   import Paragraph from "$lib/components/type/Paragraph.svelte";
   import { userEditorTheme, editorTheme } from "$lib/stores/editorThemeStore";
   import ThemeToggle from "$lib/components/buttons/ThemeToggle.svelte";
+  import SmallIconButton from "$lib/components/buttons/SmallIconButton.svelte";
+  import { Save } from "lucide-svelte";
+
+  function resetThemeToDefault() {
+    userEditorTheme.set(null);
+  }
+
+  function reloadPage() {
+    window.location.reload();
+  }
 </script>
 
 <div class="w-full h-full">
@@ -15,14 +25,31 @@
       <div class="flex justify-between items-center">
         <Paragraph className="text-type-primary">Editor theme</Paragraph>
         <input
-          class="text-type-primary block py-1 bg-mono-accentLight1 rounded-sm outline-none w-28 text-center caret-type-primary"
+          class="text-type-primary block py-1 mx-2 bg-mono-accentLight1 rounded-sm outline-none w-28 text-center caret-type-primary"
           placeholder={$editorTheme}
           bind:value={$userEditorTheme} />
+        <SmallIconButton
+          title="Save theme"
+          onClick={reloadPage}>
+          <Save />
+        </SmallIconButton>
       </div>
       <Paragraph className="text-sm">
-        All themes are from shiki, you can find them <a
+        <button
+          title="Reset theme"
+          type="button"
+          onclick={() => {
+            resetThemeToDefault();
+            reloadPage();
+          }}
+          class="text-blue-500 hover:underline">
+          Reset theme to default.
+        </button>
+        <br />
+        All themes are from shiki, you can find them
+        <a
           href="https://shiki.matsu.io/themes"
-          title="Available themes">here</a>
+          title="Available themes">here.</a>
       </Paragraph>
     </div>
   </div>

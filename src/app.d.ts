23ea8892f4650d2369adea1db5d08b5a2ffbbbb5
Cooper <Cooperransom08@gmail.com>
2024-11-20 16:35:21 +0000

    fix file formatting and clean up code

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,9 +4,6 @@ declare global {
   namespace App {
     // interface Error {}
     // interface Locals {}
-    // interface PageData {}
-    // interface PageState {}
-    // interface Platform {}
   }
 }
 
